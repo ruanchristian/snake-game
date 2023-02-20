dd76031edeb9957d34da5c5724d80ec742280df8
Ruan Christian <christianruan94@gmail.com>
2023-02-20 11:01:43 -0300

    Correção de bugs na movimentação

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const box = document.getElementById("tabuleiro");
 const h1 = document.querySelector("h1");
 
-const pSize = 29, dimension = 17, velocidade=1;
+const pSize = 19, dimension = 26, velocidade = 1;
 
 let ctx = box.getContext("2d");
 let snakeX = 10, snakeY = 15, cauda = 1;
@@ -19,9 +19,9 @@ function start() {
     // Cria a cobra
     ctx.fillStyle = "blue";
     for (let i = 0; i < trail.length; i++) {
-        ctx.fillRect(trail[i].x*pSize, trail[i].y*pSize, pSize-1, pSize-1);
+        ctx.fillRect(trail[i].x * pSize, trail[i].y * pSize, pSize-1, pSize-1);
 
-        if (cauda > 2 && trail[i].x === snakeX && trail[i].y === snakeY) {
+        if (cauda > 1 && trail[i].x === snakeX && trail[i].y === snakeY) {
             new Audio("assets/sounds/gameover.wav").play();
             velocityX = velocityY = 0;
             cauda = 1;
@@ -41,51 +41,61 @@ function start() {
 
 function validarMovimentos() {
     if (snakeX < 0) snakeX = dimension - 1;
-    if (snakeX > dimension - 1) snakeX=0;
+    if (snakeX > dimension - 1) snakeX = 0;
     if (snakeY < 0) snakeY = dimension - 1;
-    if (snakeY > dimension - 1) snakeY=0;
+    if (snakeY > dimension - 1) snakeY = 0;
 }
 
 function update(keyEvent) {
     switch (keyEvent.keyCode) {
         case 37: // Esquerda
-            velocityX = -velocidade;
-            velocityY = 0;
+            if (velocityX !== velocidade) {
+                velocityX = -velocidade;
+                velocityY = 0;
+            }
             break;
         case 38: // Cima
-            velocityX = 0;
-            velocityY = -velocidade;
+            if (velocityY !== 1) {
+                velocityX = 0;
+                velocityY = -velocidade;
+            }
             break;
         case 39: // Direita
-            velocityX = velocidade;
-            velocityY = 0;
+            if (velocityX >= 0) {
+                velocityX = velocidade;
+                velocityY = 0;
+            }
             break;
         case 40: // Baixo
-            velocityX = 0;
-            velocityY = velocidade;
+            if (velocityY >= 0) {
+                velocityX = 0;
+                velocityY = velocidade;
+            }
             break;
-         default:
+        default:
             console.table(trail);
-            break;   
+            break;
     }
 }
 
 function draw() {
     // Cria o tabuleiro com a largura e altura do canvas setadas no HTML.
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, box.width, box.height);
 
     // Cria a maçã nas posições X e Y definidas.
     ctx.fillStyle = "red";
-    ctx.fillRect(appleX*pSize, appleY*pSize, pSize, pSize);
+    ctx.fillRect(appleX * pSize, appleY * pSize, pSize, pSize);
 }
 
 window.onload = () => {
     document.addEventListener("keydown", update);
     setInterval(start, 90);
 
-    // Mudar cor do texto central a cada 300 millisegundos
+    // Mudar cor do texto central a cada 450 millisegundos
+    let aux = 0;
+    let rgb = ["#FF0000", "#008000", "#0000FF"];
     setInterval(() => {
-        h1.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
-    }, 300);
+        h1.style.color = rgb[aux++ % 3];
+    }, 450);
 }
